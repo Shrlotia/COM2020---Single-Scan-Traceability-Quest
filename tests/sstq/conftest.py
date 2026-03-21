f10dfde2fb0e3ae33caeabf36a2e32088eeee1a6
johnny371123 <johnny371123@gmail.com>
2026-03-21 12:43:10 +0800

    Refactor pytest fixtures for app and client

--- tests/sstq/conftest.py
+++ tests/sstq/conftest.py
@@ -1,18 +1,16 @@
 import pytest
-import sstq.main
 from sstq.main import app, db
 from sstq.models import User
 
-@pytest.fixture
-def client():
 
+@pytest.fixture
+def app_instance():
     app.config.update({
         "TESTING": True,
         "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
     })
-   
+
     with app.app_context():
-        #make sure it will be a flash db
         db.drop_all()
         db.create_all()
 
@@ -22,13 +20,23 @@ def client():
         db.session.add(user)
         db.session.commit()
 
-    with app.test_client() as client:
-        #simulated login - login via actual login endpoint
-        with client.session_transaction() as sess:
-            sess["_user_id"] = "1"
-        
-        yield client
+    yield app
 
     with app.app_context():
         db.session.remove()
         db.drop_all()
+
+
+@pytest.fixture
+def client(app_instance):
+    return app_instance.test_client()
+
+
+@pytest.fixture
+def logged_in_client(client):
+    client.post("/login", data={
+        "username": "testuser",
+        "password": "1234",
+        "action": "login"
+    })
+    return client
